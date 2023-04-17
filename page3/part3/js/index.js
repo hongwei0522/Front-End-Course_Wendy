f867a6ab0e4b0ba73bf9966c9cfee0d519f62f33
Wendy Chang <wendy950210@gmail.com>
2023-04-17 12:50:58 +0800

    second commit index.html

--- page3/part3/js/index.js
+++ page3/part3/js/index.js
@@ -34,6 +34,8 @@ animationOut.onfinish = () => {
   animation.style.display = 'none';
 };
 
+
+
 // .test-go 測驗
   const testGo = document.querySelectorAll('.test-go')
   const testContainer = document.querySelector('.testContainer ');
@@ -61,6 +63,7 @@ animationOut.onfinish = () => {
   let pieNum = document.querySelector('.pieNum');
   let urlButton = document.getElementById("newURL");
 
+  // 五個題目
   let quizData = [
     {
       question: "選擇在哪座城市學習",
@@ -109,9 +112,9 @@ animationOut.onfinish = () => {
   },
   ];
 
-  
-const url = '../json/front-enter-export.json';
-let articles = {};
+  // 整理出九個物件的屬性
+  const url = '../json/front-enter-export.json';
+  let articles = {};
 
   fetch(url)
   .then(res => res.json())
@@ -132,314 +135,367 @@ let articles = {};
     localStorage.setItem('articles', JSON.stringify(articles));
   });
 
+  // 將 articles 存在瀏覽器
   const storedArticles = localStorage.getItem('articles');
   if (storedArticles) {
     articles = JSON.parse(storedArticles);
   }
-  console.log(articles);
 
+  // 比較屬性與題目的相符程度
   compareSchools(quizData, articles);
   let answerData = compareSchools(quizData, articles);
+
   console.log(answerData);
-  
-  
-function compareSchools(quizData, articles) {
-  let answerData = [];
-  articles.forEach(school => {
-    let answers = []; // 用來存放符合的選項的陣列
-    // 比較 cityName
-    quizData.forEach(question => {
-      if (question.questionNum === '1/5') {
-        let option = Object.keys(question).find(key => question[key] === school.cityName);
-        answers.push(option);
-      }
-    });
-    // 比較 fee
-    quizData.forEach(question => {
-      if (question.questionNum === '2/5') {
-        let option;
-        let fee = parseInt(school.fee);
-        if (fee<= 3000) {
-          option = "1";
-        } else if (fee > 3000 && fee <= 6000) {
-          option = "2";
-        } else if (fee > 6000 && fee <= 10000) {
-          option = "3";
-        } else {
-          option = "4";
-        }
+
+  function compareSchools(quizData, articles) {
+    let answerData = [];
+    articles.forEach(school => {
+      let answers = []; 
+      quizData.forEach(question => {
+        if (question.questionNum === '1/5') {
+          let option = Object.keys(question).find(key => question[key] === school.cityName);
           answers.push(option);
         }
-    });
-    
-    // 比較 weekHour
-    quizData.forEach(question => {
-      if (question.questionNum === '3/5') {
-        let option;
-        let weekHour = parseInt(school.weekHour);
-        if (weekHour<= 16) {
-          option = "1";
-        } else if (weekHour > 16 && weekHour <= 30) {
-          option = "2";
-        } else if (weekHour > 30 && weekHour <= 45) {
-          option = "3";
-        } else {
-          option = "4";
-        }
+      });
+      quizData.forEach(question => {
+        if (question.questionNum === '2/5') {
+          let option;
+          let fee = parseInt(school.fee);
+          if (fee<= 3000) {
+            option = "1";
+          } else if (fee > 3000 && fee <= 6000) {
+            option = "2";
+          } else if (fee > 6000 && fee <= 10000) {
+            option = "3";
+          } else {
+            option = "4";
+          }
+            answers.push(option);
+          }
+      });
+      quizData.forEach(question => {
+        if (question.questionNum === '3/5') {
+          let option;
+          let weekHour = parseInt(school.weekHour);
+          if (weekHour<= 16) {
+            option = "1";
+          } else if (weekHour > 16 && weekHour <= 30) {
+            option = "2";
+          } else if (weekHour > 30 && weekHour <= 45) {
+            option = "3";
+          } else {
+            option = "4";
+          }
+            answers.push(option);
+          }
+      });
+      quizData.forEach(question => {
+        if (question.questionNum === '4/5') {
+          let option = Object.keys(question).find(key => question[key] === school.classType);
           answers.push(option);
         }
-});
-    // 比較 class type
-    quizData.forEach(question => {
-      if (question.questionNum === '4/5') {
-        let option = Object.keys(question).find(key => question[key] === school.classType);
-        answers.push(option);
-      }
+      });
+        // 比較 teach way
+      quizData.forEach(question => {
+        if (question.questionNum === '5/5') {
+          let option = Object.keys(question).find(key => question[key] === school.teachWay);
+          answers.push(option);
+        }
+      });
+      // 將結果加入 answerData
+      answerData.push({ className: school.className, options: answers, id: school.id });
     });
-      // 比較 teach way
-    quizData.forEach(question => {
-      if (question.questionNum === '5/5') {
-        let option = Object.keys(question).find(key => question[key] === school.teachWay);
-        answers.push(option);
-      }
-    });
-    // 將結果加入 answerData
-    answerData.push({ className: school.className, options: answers });
-  });
-  return answerData;
-}
-
-
-
-
-  let pageNum = 0;
-  console.log(pageNum)
-
-  if(pageNum === 0){
-    testGo.forEach((e)=>{
-    e.addEventListener('click', function(){
-      testContainer.style.display = 'flex';
-      testHeader.style.display = 'flex';
-      testIntro.style.display = 'flex';
-      ul.style.display = 'none';
-      pageNum = 1;
-      console.log(pageNum);
-    })
-    
-  })
+    return answerData;
   }
 
-  testBackground.addEventListener('click', function(event){
-    if (event.target === testBackground) {
-      setTimeout(function() {
-        testContainer.style.display = 'none';
-        testQuestion.style.display = 'none';
-        pieContainer.style.display = 'none';
-        urlButton.style.display = 'none'
-        pageNum = 0;
-        console.log(pageNum);
-      }, 0);
-    }
-  });
   
-
-  
-
-let currentQuiz = 0; //當前測驗
-let score = 0;
-
-
-function loadQuiz() {
-  deselectAnswer();
-  let currentQuizData = quizData[currentQuiz];
-  testQuestion.innerText = currentQuizData.question;
-  testNumber.innerText = currentQuizData.questionNum; 
-  oneText.innerText = currentQuizData[1];
-  twoText.innerText = currentQuizData[2];
-  threeText.innerText = currentQuizData[3];
-  fourText.innerText = currentQuizData[4];
-  fiveText.innerText = currentQuizData[5];
-}
-
-function deselectAnswer() {
-  answerEls.forEach((answerEl) => {
-    answerEl.checked = false;
-  });
-}
-
-let answers = [];
-
-// 檢查哪一個選項被選中
-function getSelected() {
-  answerEls.forEach((answerEl) => {
-    if (answerEl.checked) {
-      answers.push(answerEl.id);
+    let currentQuiz = 0; //當前測驗
+    let score = 0;
+    let answers = [];
+    
+    function loadQuiz() {
+      deselectAnswer();
+      let currentQuizData = quizData[currentQuiz];
+      testQuestion.innerText = currentQuizData.question;
+      testNumber.innerText = currentQuizData.questionNum; 
+      oneText.innerText = currentQuizData[1];
+      twoText.innerText = currentQuizData[2];
+      threeText.innerText = currentQuizData[3];
+      fourText.innerText = currentQuizData[4];
+      fiveText.innerText = currentQuizData[5];
     }
-  });
-  return answers;
-}
+    
+    function deselectAnswer() {
+      answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+      });
+    }
+    
+    // 檢查哪一個選項被選中
+    function getSelected() {
+      answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+          answers.push(answerEl.id);
+        }
+      });
+      return answers;
+    }
 
-let percentage;
-var beforeRotation; 
-var afterRotation;
-var lightBlue = '#1ad8d3';
-var darkBlue = '#1cb5e0';
-var beforeBackgroundColor = lightBlue;
-var afterBackgroundColor = lightBlue;
 
-pieChart.style.setProperty('--before-background-color', beforeBackgroundColor);
-pieChart.style.setProperty('--after-background-color', afterBackgroundColor);
+    // 中途停止
+    function testEnd(){
+    testBackground.addEventListener('click', function(event){
+      if (event.target === testBackground) {
+        setTimeout(function() {
+          currentQuiz = 0;
+          percentage = 0;
+          testContainer.style.display = 'none';
+          testQuestion.style.display = 'none';
+          testNumber.style.display = 'none';
+          pieContainer.style.display = 'none';
+          urlButton.style.display = 'none'
+          answers = [];
+          console.log(percentage);
+        }, 0);
+      }
+      else if(event.target === boxContainer){('click', function(event) {event.stopPropagation()})
+    }
+    })
+  }
 
-// 依照 percentage 設定旋轉度數 
-  function rotateDeg(){
-      if(percentage === 20){
+
+    testEnd();
+
+
+// 隨機數字與旋轉角度
+    var beforeRotation; 
+    var afterRotation;
+    var lightBlue = '#1ad8d3';
+    var darkBlue = '#1cb5e0';
+    var beforeBackgroundColor = lightBlue;
+    var afterBackgroundColor = lightBlue;
+
+    pieChart.style.setProperty('--before-background-color', beforeBackgroundColor);
+    pieChart.style.setProperty('--after-background-color', afterBackgroundColor);
+
+    // 依照 percentage 設定旋轉度數 
+    function rotateDeg() {
+      console.log(percentage);
+      if (percentage >= 0 && percentage < 20) {
+        beforeRotation = 0;
+        afterRotation = 0;
+      } else if (percentage >= 20 && percentage < 40) {
         beforeRotation = 60;
         afterRotation = 186;
-  }else if(percentage === 40){
+      } else if (percentage >= 40 && percentage < 60) {
         beforeRotation = 130;
         afterRotation = 180;
-  }else if(percentage === 60){
+      } else if (percentage >= 60 && percentage < 80) {
         beforeRotation = 40;
         afterRotation = 0;
         pieChart.style.backgroundColor = lightBlue;
         beforeBackgroundColor = darkBlue;
         afterBackgroundColor = darkBlue;
-  }else if(percentage === 80){
+      } else if (percentage >= 80 && percentage < 100) {
         beforeRotation = 40;
         afterRotation = -50;
         pieChart.style.backgroundColor = lightBlue;
         beforeBackgroundColor = darkBlue;
         afterBackgroundColor = darkBlue;
-  }else if(percentage === 100){
-        beforeRotation = 0;
-        afterRotation = 0;
-        beforeBackgroundColor = darkBlue;
-        afterBackgroundColor = darkBlue;
-}
-  pieChart.style.setProperty('--before-rotation', beforeRotation + 'deg');
-  pieChart.style.setProperty('--after-rotation', afterRotation + 'deg');
-  pieChart.style.setProperty('--before-background-color', beforeBackgroundColor);
-  pieChart.style.setProperty('--after-background-color', afterBackgroundColor);
-}
-
-// 隨機動畫
-    let updatePieCount = 1;
-    function updatePieNum() {
-      if (updatePieCount <= 9) {
-        const randomNum = Math.floor(Math.random() * 101);
-        const randomClassNum = Math.floor(Math.random() * articles.length); 
-        pieNum.textContent = randomNum + '%';
-        urlButton.textContent = articles[randomClassNum].className;
-        updatePieCount++;
-        setTimeout(updatePieNum, 300);
-      } else {
-        pieNum.textContent = `${percentage}%`; 
-      }
+      }else if (percentage == 100){
+            beforeRotation = 0;
+            afterRotation = 0;
+            beforeBackgroundColor = darkBlue;
+            afterBackgroundColor = darkBlue;
+    }
+      pieChart.style.setProperty('--before-rotation', beforeRotation + 'deg');
+      pieChart.style.setProperty('--after-rotation', afterRotation + 'deg');
+      pieChart.style.setProperty('--before-background-color', beforeBackgroundColor);
+      pieChart.style.setProperty('--after-background-color', afterBackgroundColor);
     }
 
+    // 從首頁開啟測驗介紹
+    testGo.forEach((e)=>{
+        e.addEventListener('click', function(){
+          currentQuiz = 0;
+          testContainer.style.display = 'flex';
+          testHeader.style.display = 'flex';
+          testIntro.style.display = 'flex';
+          startBtn.style.display = 'flex';
+          ul.style.display = 'none';
+          console.log(currentQuiz);
+        })
+        
+      })  
+  
+    // 從測驗介紹開始測驗
+      startBtn.addEventListener('click', function(){
+        currentQuiz = 0;
+        testHeader.style.display = 'none';
+        testIntro.style.display = 'none';
+        this.style.display = 'none';
+        testQuestion.style.display = 'flex';
+        testNumber.style.display = 'flex';
+        ul.style.display = 'flex';
+        loadQuiz();
+        console.log(currentQuiz);
+      })
+  
 
-startBtn.addEventListener('click', function(){
-  testHeader.style.display = 'none';
-  testIntro.style.display = 'none';
-  this.style.display = 'none';
-  testQuestion.style.display = 'flex';
-  testNumber.style.display = 'flex';
-  ul.style.display = 'flex';
+      
+      // 隨機動畫之後顯示結果
+      function randomPercentage() {
+        return Math.floor(Math.random() * 101) + '%'; 
+      }
+      function randomClassNum(){
+        return Math.floor(Math.random() * articles.length);
+      }
 
-  loadQuiz();
+      function changePercentage() {
+        var intervalId = setInterval(function() {
+          pieNum.textContent = randomPercentage(); 
+          urlButton.textContent = articles[randomClassNum()].className
+        }, 250); // 每 0.3 秒變換一次數字
+        setTimeout(function() {
+          clearInterval(intervalId); 
+          pieNum.textContent = `${percentage}%`;
+          urlButton.textContent = `${matchedSchool}`;
+          rotateDeg();
+          urlButton.addEventListener('click',()=>{
+            window.location.href = newURL;
+          })
+          console.log(newURL);
+        }, 4000);
+      }
+      
 
-  answerEls.forEach(e=>e.addEventListener("click", (event) => {
-    event.stopPropagation();
-    let answers = getSelected(); 
-    console.log(answers);
-    currentQuiz++;
+      let percentage = 0; 
+      let matchedSchool = ''; 
+      let id = '';
+      let newURL = '';
+    
+      
 
-    if (currentQuiz < quizData.length) {
-      loadQuiz();
-    } else {
-      let maxScore = 0;
-      let matchedSchool = '';
-
-
-      answerData.forEach((school) => {
-        let score = 0;
-        for (let i = 0; i < answers.length; i++) {
-          if (answers[i] === school.options[i]) {
-            score++;
-          }
+      // 按下選項
+      answerEls.forEach(e=>e.addEventListener("click", (event) => {
+        event.stopPropagation();
+        let answers = getSelected(); 
+        console.log(answers);
+        console.log(currentQuiz);
+        
+        currentQuiz++;
+      
+        if (currentQuiz < quizData.length) {
+          setTimeout(() => {
+            loadQuiz();
+          }, 100);
+        } else {
+          let maxScore = 0;
+          testQuestion.innerText = '你有多適合下列學校呢？';
+          testNumber.style.display = 'none';
+          ul.style.display = 'none';
+          pieContainer.style.display = 'flex';
+          urlButton.style.display = 'flex';
+          answerData.forEach((school) => {
+            let score = 0;
+            
+            for (let i = 0; i < answers.length; i++) {
+              if (answers[i] === school.options[i]) {
+                score++;
+              }
+            }
+              if (score > maxScore) {
+              maxScore = score;
+              matchedSchool = school.className;
+              id = school.id;
+              newURL = `content.html?id=${id}`;
+              percentage = ((score / answers.length) * 100).toFixed(0);
+              }
+            })
+            changePercentage();
+            console.log(percentage);
+            console.log(matchedSchool); 
+            console.log(id); 
         }
-          if (score > maxScore) {
-          maxScore = score;
-          matchedSchool = school.className;
-          percentage = (score / answers.length) * 100;
-        }
-      });
-
-      updatePieNum();
-      setTimeout(updatePieNum, 0);
-
-      testQuestion.innerText = '你有多適合下列學校呢？';
-      testNumber.style.display = 'none';
-      ul.style.display = 'none';
-      pieContainer.style.display = 'flex'
-      urlButton.style.display = 'flex'
-      rotateDeg();
-      urlButton.textContent = `${matchedSchool}`;
-    }  
-  }))
-
-})
-
+      }
+   ))
 
 
 // 轉到 article.html
 
-const searchButton = document.getElementById('search-button');
+  const searchButton = document.getElementById('search-button');
 
-searchButton.addEventListener('click', function() {
-  window.location.href = 'article.html';
-});
+  searchButton.addEventListener('click', function() {
+    window.location.href = 'article.html';
+  });
 
-const logoBtn = document.querySelector('.logo');
-logoBtn.addEventListener('click', function() {
-  window.location.href = 'index.html';
-});
+  const logoBtn = document.querySelector('.logo');
+  logoBtn.addEventListener('click', function() {
+    window.location.href = 'index.html';
+  });
 
 // Top Button
 
-const scrollBtn = document.getElementById("topwrap");
+  const scrollBtn = document.getElementById("topwrap");
 
-scrollBtn.addEventListener("click", function() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-});
+  scrollBtn.addEventListener("click", function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
 
 
 // search-button
 
-let searchBox = document.getElementById('search-box');
+  let searchBox = document.getElementById('search-box');
 
-let blackBox = document.getElementById('black-box');
+  let blackBox = document.getElementById('black-box');
 
-let clickGlass = document.getElementById('click-glass');
+  let clickGlass = document.getElementById('click-glass');
 
-let clickCount = 0;
+  let clickCount = 0;
 
-clickGlass.addEventListener('click', function() {
-  clickCount += 1;
-  if (clickCount === 1) {
-  searchBox.style.display = 'flex';
-  blackBox.style.display = 'flex';}
-  if (clickCount === 2) {
-  searchBox.style.display = 'none';
-  blackBox.style.display = 'none';
-  clickCount = 0;}
-});
+  clickGlass.addEventListener('click', function() {
+    clickCount += 1;
+    if (clickCount === 1) {
+    searchBox.style.display = 'flex';
+    blackBox.style.display = 'flex';}
+    if (clickCount === 2) {
+    searchBox.style.display = 'none';
+    blackBox.style.display = 'none';
+    clickCount = 0;}
+  });
 
-blackBox.onclick = () => {
-  searchBox.style.display = 'none';
-  blackBox.style.display = 'none';
-};
+  blackBox.onclick = () => {
+    searchBox.style.display = 'none';
+    blackBox.style.display = 'none';
+  };
 
-// 
+  // function searchBarText(value){
+  //   searchInput.addEventListener('keydown', e => {
+  //     if (e.keyCode === 13) {
+  //       e.preventDefault();
+  //       searchBox.style.display = 'none';
+  //       blackBox.style.display = 'none';
+  //       const value = e.target.value.toLowerCase().trim();
+  //       articles.forEach(object => {
+  //         // 若為空白則全部顯示
+  //         const isVisible = (object.cityName.toLowerCase().includes(value) || object.classroomName.toLowerCase().includes(value) || object.preface.toLowerCase().includes(value)) && (value.length > 0);
+  //         object.element.style.display = isVisible ? 'flex' : 'none';
+  //         });
+  //       }
+  //   })
+  // }
+  
+  //   searchGlass.addEventListener('click', e => {
+  //     e.preventDefault();
+  //     searchBox.style.display = 'none';
+  //     blackBox.style.display = 'none';
+  //     const value = searchInput.value.toLowerCase().trim();
+  //     articles.forEach(object => {
+  //       const isVisible = (object.cityName.toLowerCase().includes(value) || object.classroomName.toLowerCase().includes(value) || object.preface.toLowerCase().includes(value)) && (value.length > 0);
+  //       object.element.style.display = isVisible ? 'flex' : 'none';
+  //       })
+  //   })
+  
+  
 
